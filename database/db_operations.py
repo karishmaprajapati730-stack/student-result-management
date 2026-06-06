f871363db2fcb6a88cf442d82b7fca402cf8dec3
karishmaprajapati473-max <karishmaprajapati473@gmail.com>
2026-06-06 18:54:04 +0530

    adding data

--- database/db_operations.py
+++ database/db_operations.py
@@ -4,16 +4,31 @@ import sqlite3
 
 conn=sqlite3.connect("student.db")
 
-try:
-    conn.execute('''create table student(st_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                 st_name TEXT,
-                 st_class TEXT,
-                 maths INTEGER,
-                 science INTEGER,
-                 english INTEGER)
-                 ''')
+# try:
+#     conn.execute('''create table student(st_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                  st_name TEXT,
+#                  st_class TEXT,
+#                  maths INTEGER,
+#                  science INTEGER,
+#                  english INTEGER)
+#                  ''')
     
-except:
-    print("Error...")
+# except:
+#     print("Error...")
 
-    
+# conn.execute('''INSERT INTO student (st_name, st_class, maths, science, english) VALUES
+#     ('Aarav Sharma', 'BTech-CSE', 85, 78, 92),
+#     ('Priya Singh', 'BTech-CSE', 91, 88, 84),
+#     ('Rahul Verma', 'BTech-CSE', 76, 82, 79),
+#     ('Sneha Gupta', 'BTech-CSE', 95, 93, 97),
+#     ('Vikram Patel', 'BTech-CSE', 68, 74, 70),
+#     ('Ananya Mishra', 'BTech-CSE', 89, 90, 86),
+#     ('Rohan Kumar', 'BTech-CSE', 81, 77, 83)
+# ''')
+
+
+data = conn.execute("SELECT * FROM student").fetchall()
+
+conn.commit()
+conn.close()
+
