ad948d033faf6a82a3b03b0b454ae1af224c8c60
karishmaprajapati473-max <karishmaprajapati473@gmail.com>
2026-06-06 18:40:56 +0530

    creating database

---
+++ database/db_operations.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+# from concurrent.interpreters import create
+
+conn=sqlite3.connect("student.db")
+
+try:
+    conn.execute('''create table student(st_id INT AUTO_INCREMENT,
+                 st_name VARCHAR(50),
+                 st_class VARCHAR(10),
+                 st_email VARCHAR(30))
+                 ''')
+    
+except:
+    print("Error...")
